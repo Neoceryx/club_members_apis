1226c05f72faeaa1cf0bbd2c82fd07a111ff466c
xion93 <dani-deoz@hotmail.com>
2023-10-30 20:35:43 -0700

    Model for Members and Charges were created

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, Date, Boolean
 from db.database import Base
 
 
@@ -9,3 +9,24 @@ class User(Base):
     name = Column(String)
     email = Column(String)
     nickname = Column(String)
+
+
+class Charges(Base):
+    __tablename__ = "charges"
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String, unique=True, nullable=False)
+
+class Members(Base):
+    __tablename__ = "members"
+
+    id = Column(Integer, primary_key=True, index=True)
+    fullname = Column(String, unique=True, nullable=False)
+    blood_type = Column(String)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    phone_number = Column(String, nullable=False)
+    image = Column(String)
+    birthdate = Date
+    is_active = Boolean
+    register_date = DateTime
