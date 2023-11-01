5d1b156d1ccc2a26ca298609bdae8199fd5c9798
xion93 <dani-deoz@hotmail.com>
2023-11-01 14:13:37 -0700

    Create member funciton, fst version was created

--- routes/members.py
+++ routes/members.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+import datetime
 
 from schemas.member_sh import member_schema
 from db.models import Members
@@ -18,5 +19,25 @@ async def get_all(db: Session = Depends(get_db)):
 
 
 @member_rute.post("/")
-async def get_by_id_and_password(new_member: member_schema, db: Session = Depends(get_db)):
+async def create_new(new_member: member_schema, db: Session = Depends(get_db)):
+
+    member = Members()
+
+    member.charges_id = new_member.charges_id
+    member.fullname = new_member.fullname
+    member.blood_type = new_member.blood_type
+    member.email = new_member.email
+    member.password = new_member.password
+    member.address = new_member.address
+    member.phone_number = new_member.phone_number
+    member.image = new_member.image
+    member.birthdate = new_member.birthdate
+    member.is_active = new_member.is_active
+    member.register_date = datetime.datetime.now()
+
+    # save it on DB
+    db.add(member)
+    db.commit()
+    db.refresh(member)
+
     return {"response": "helo"}
--- schemas/member_sh.py
+++ schemas/member_sh.py
@@ -2,15 +2,13 @@ from pydantic import BaseModel
 
 
 class member_schema(BaseModel):
-    id = int
-    charges_id = int
-    fullname = str
-    blood_type = str
-    email = str
-    password = str
-    address = str
-    phone_number = str
-    image = str
-    birthdate = str
-    is_active = str
-    register_date = str
+    charges_id: int
+    fullname: str
+    blood_type: str
+    email: str
+    password: str
+    address: str
+    phone_number: str
+    image: str
+    birthdate: str
+    is_active: bool
