16f6ab62b9edc544c0a8d3d4316c6450e76cd0d8
xion93 <dani-deoz@hotmail.com>
2023-11-03 13:07:54 -0700

    Method to save new member was created

--- repository/membersDAL.py
+++ repository/membersDAL.py
@@ -1,10 +1,11 @@
+from datetime import datetime
+
 from sqlalchemy.orm import Session
 from db.models import Members
+from schemas.member_sh import member_schema
 
 
 # This class contains All the Operation related with the Data Base
-
-
 class members_Dal:
 
     def get_all(self, db: Session):
@@ -15,6 +16,30 @@ class members_Dal:
 
     def is_email_registered(self, email: str, db: Session):
         return db.query(Members).filter(Members.email == email).count()
+
+    def new_member(self, new_member: member_schema, db: Session):
+
+        member = Members()
+
+        member.charges_id = new_member.charges_id
+        member.fullname = new_member.fullname
+        member.blood_type = new_member.blood_type
+        member.email = new_member.email
+        member.password = new_member.password
+        member.address = new_member.address
+        member.phone_number = new_member.phone_number
+        member.image = new_member.image
+        member.birthdate = new_member.birthdate
+        member.is_active = new_member.is_active
+        member.register_date = datetime.datetime.now()
+
+        # save it on DB
+        db.add(member)
+        db.commit()
+        db.refresh(member)
+
+        qqq = member.id
+        pass
 
     pass
     # end class
