7ccf2e7c514549129b745b2e82cc44e60fca28bd
xion93 <dani-deoz@hotmail.com>
2023-11-04 11:53:55 -0700

    Sentry was implemeted over a test api

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI, Depends
 import uvicorn
+import sentry_sdk
 
 # To import customs apis
 from routes.members import member_rute
 from db.database import engine, Base
+
+sentry_sdk.init(
+    dsn="https://f3acb52326ccebaaccbefdcdcaea0472@o203672.ingest.sentry.io/4506168974901248",
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
 
 # to create the models on the DB
 Base.metadata.create_all(bind=engine)
@@ -11,6 +18,10 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(member_rute)
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
 
 
 # Press the green button in the gutter to run the script.
