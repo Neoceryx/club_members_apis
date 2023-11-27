84bc34e97615e68b29e23777b9f474ddc3c23299
xion93 <dani-deoz@hotmail.com>
2023-11-26 19:41:28 -0800

    method to save new charges, has been created

--- repository/charges_DAL.py
+++ repository/charges_DAL.py
@@ -1,11 +1,24 @@
 from sqlalchemy.orm import Session
 from db.models import Charges
+from schemas.charges import charge_schema
 
 
 class charge_DAL:
 
     def get_all(self, db: Session):
         return db.query(Charges).all()
+
+    def save_new_charge(self, new_charge: charge_schema, db: Session):
+        charge = Charges()  # create Entity Object
+
+        charge.description = new_charge.description
+
+        # save it on BD
+        db.add(charge)
+        db.commit()
+        db.refresh(charge)
+
+        return charge.id  # it will return the new charge saved ID
 
     pass
     # end class
