36c34137a2bba20a4aed50835d45ea647641cac2
xion93 <dani-deoz@hotmail.com>
2023-11-26 19:41:59 -0800

    method to save new charges , has been created in Service layer

--- services/charges_BLL.py
+++ services/charges_BLL.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from repository.charges_DAL import charge_DAL
-
+from schemas.charges import charge_schema
 
 class charge_BLL:
     charge_DAL = None
@@ -11,6 +11,16 @@ class charge_BLL:
 
     def get_charges(self, db: Session):
         return self.charge_DAL.get_all(db)
+
+    def new_charge(self, new_charge: charge_schema, db: Session):
+        # TODO: Create a common class, with the method to return a correct format for strings, in Camel Case
+        new_charge.description = new_charge.description.title()
+
+
+
+
+
+
 
     pass
     # end charge_BLL class
