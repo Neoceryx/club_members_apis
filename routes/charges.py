1150086f0ffd8ea4af6cf92e5056a1d9b31f002b
xion93 <dani-deoz@hotmail.com>
2023-11-26 19:40:51 -0800

    New route to save new charges has been created

--- routes/charges.py
+++ routes/charges.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from db.database import get_db
 from sqlalchemy.orm import Session
 from services.charges_BLL import charge_BLL
+from schemas.charges import charge_schema
 
 charge_rute = APIRouter(
     prefix="/charges",
@@ -10,7 +11,12 @@ charge_rute = APIRouter(
 
 charge_bll = charge_BLL()
 
+
 @charge_rute.get("/")
 async def get_charges(db: Session = Depends(get_db)):
     return charge_bll.get_charges(db)
 
+
+@charge_rute.post("/")
+async def create_newone(new_charge: charge_schema, db:Session = Depends(get_db)):
+    return charge_bll.new_charge(new_charge, db)
